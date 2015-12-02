e54f672e72ba92783e620954159c27d3bf2d521f
wally800 <joshuajw@princeton.edu>
2015-12-02 12:19:21 -0500

    some ce output removed, next will work on removing it completely

--- compile.sh
+++ compile.sh
@@ -4,11 +4,11 @@ gfortran element6.for -o element6
 gfortran close6.for -o close6
 gfortran mercury6_2.for -o mercury6
 
-echo "Create sample data files? (yes/no)"
-read create_samples
+#echo "Create sample data files? (yes/no)"
+#read create_samples
 
-if [ $create_samples = 'yes' ] ; then
-  for i in *.sample; do
-    cp $i `echo $i | sed 's/.sample//g'`
-  done
-fi
+#if [ $create_samples = 'yes' ] ; then
+#  for i in *.sample; do
+#    cp $i `echo $i | sed 's/.sample//g'`
+#  done
+#fi
