63775a2767f16eaec5c3d6358bb7781fa6883152
wally800 <joshuajw@princeton.edu>
2016-06-08 11:30:29 -0400

    changed compilation script to include condor_compile command

--- compile_merc7.sh
+++ compile_merc7.sh
@@ -1,6 +1,12 @@
 echo "Compiling mercury7 package"
+echo "First for Condor"
+condor_compile gfortran -w  -O3 -o mercury7 frag.f90
 
-gfortran -w -O3 -o element7 element7.for 
-gfortran -w  -O3 -o mercury7 frag.f90
+echo "Now not for Condor"
+gfortran -w  -O3 -o mercury7_nocondor frag.f90
+
+#echo "Now element7"
+#gfortran -w -O3 -o element7 element7.for 
+
 #gfortran -w  -O3 -o mercury7_noclo frag.noclo.f90
 
