4a4bbe0daca74a60fd4d014881d09d0cf0912da3
wally800 <joshuajw@princeton.edu>
2016-11-02 18:01:39 -0400

    added in no tidal modification frag.f90

--- compileandmove.sh
+++
@@ -1,6 +0,0 @@
-./compile.sh
-./compile_merc7.sh
-cp -f mercury7 ../testing_mercury_output/mercury7/
-cp -f mercury6 ../testing_mercury_output/mercury6/
-cp -f mercury7 ../testing_mercury_output/mercury7_withfrag/
-cp -f mercury6 ../testing_mercury_output/mercury6_withfrag/
